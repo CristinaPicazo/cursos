618e106d92aab624f5c8e60311039fda3e04674e
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2025-02-18 17:21:48 +0100

    archivos texto

--- js/cursos.js
+++ js/cursos.js
@@ -1,14 +1,16 @@
+let listaCursos = [];
+
 function DescargarCursoJSON() {
   // Extraer valores de los inputs
   let nombre = document.getElementById("nombre").value;
   let escuela = document.getElementById("escuela").value;
   let tecnologia = document.getElementById("tecnologia").value;
-  let fechaInicio = document.getElementById("fechaInicio").value;
+  let fecha = document.getElementById("fecha").value;
   let enlace = document.getElementById("enlace").value;
 
   //   Crear un array con los valores
   let cursos = new Array();
-  cursos.push([nombre, escuela, tecnologia, fechaInicio, enlace]);
+  cursos.push([nombre, escuela, tecnologia, fecha, enlace]);
 
   //   Convertir a JSON
   let json = JSON.stringify(cursos);
@@ -22,7 +24,7 @@ function DescargarCursoJSON() {
   var url = window.URL || window.webkitURL;
   let link = url.createObjectURL(blob);
   var a = document.createElement("a");
-  a.download = "Cursos.txt";
+  a.download = "Cursos.json";
   a.href = link;
   document.body.appendChild(a);
   a.click();
@@ -38,7 +40,6 @@ function DescargarCursoJSON() {
 
   //   window.navigator.msSaveBlob(blob, "Cursos.txt");
 
-  
   //   //Check the Browser.
   //   let isIE = false || !!document.documentMode;
   //   if (isIE) {
@@ -56,9 +57,46 @@ function DescargarCursoJSON() {
 let guardarCurso = document.getElementById("guardarCurso");
 guardarCurso.addEventListener("click", DescargarCursoJSON);
 
-// function mostrarCursos() {
-//   cursos.style.display = "block";
-// }
+function mostrarCursos() {
+  let nombreCurso = document.getElementById("nombreCurso");
+  let escuelaCurso = document.getElementById("escuelaCurso");
+  let tecnologiaCurso = document.getElementById("tecnologiaCurso");
+  let fechaCurso = document.getElementById("fechaCurso");
+  let enlaceCurso = document.getElementById("enlaceCurso");
+
+  fetch("./listaCursos/cursos.json")
+    .then((response) => response.text())
+    .then((data) => {
+      let cursos = data.replace(/\\/g, "");
+      console.log('cursos', cursos)
+      cursos = cursos.replace(/\[/g, "");
+      cursos = cursos.replace(/\]/g, "");
+      cursos = cursos.replace(/\"/g, "");
+      cursos = cursos.split(",");
+
+      console.log("cursos", cursos);
+
+      let td = document.createElement("td");
+      cursos.forEach((curso) => {
+        listaCursos.push(curso);
+        // console.log('curso', curso)
+
+        // td.textContent = curso;
+        // nombreCurso.appendChild(td);
+
+        // let cursoArray = curso.split(",");
+        // let li = document.createElement("li");
+        // li.textContent = cursoArray[0];
+        // listaCursos.appendChild(li);
+      });
+    });
+  cursos.style.display = "block";
+}
+console.log("listaCursos", listaCursos);
+console.log("listaCursos", typeof listaCursos);
+console.log("listaCursos", typeof listaCursos[0]);
+
+mostrarCursos();
 
 // function guardarCurso() {
 //   let nombreCurso = document.getElementById("nombreCurso").value;
